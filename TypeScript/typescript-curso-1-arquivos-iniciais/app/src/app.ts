f17211f6c47f5f49668594d729dd701f8e5c04e6
Samuel Silva de Lima <samuclient@gmail.com>
2025-03-13 16:13:17 -0300

    feat: organização das pastas src, dist e app

--- TypeScript/typescript-curso-1-arquivos-iniciais/app/src/app.ts
+++ TypeScript/typescript-curso-1-arquivos-iniciais/app/src/app.ts
@@ -1,7 +1,6 @@
 /* função que faz o botão incluir adicionar as informações */
 
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
-import { NegociacaoView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
